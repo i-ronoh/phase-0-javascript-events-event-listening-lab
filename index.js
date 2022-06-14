ac9bb8fdfb141e61ebd0c148575da109b5f18783
+ <ronoh.ivy@gmail.com>
2022-06-14 23:58:45 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const clickAlert = function(){
+    alert("You\'ve clicked on input");
+} 
+
 function addingEventListener() {
-}
+    const input = document.getElementById("input");
+    return input.addEventListener("click", clickAlert);
+};
+
+addingEventListener();
